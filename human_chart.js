aa6fbb0381ea056446207e48646ef2debe6da6a7
Francesco De Luca <francy.delc01@gmail.com>
2025-05-18 17:21:24 +0200

    Primo disegno degli omini
    Aggiunto il disegno delle gambe (rettangolo diviso in due da una linea verticale).

--- human_chart.js
+++ human_chart.js
@@ -1,56 +1,97 @@
-// Setting some values for svg and axes
-var margin = {top: 40, right: 40, bottom:40, left: 40};
+// Some values to draw the svg object
+var margin = { top: 40, right: 40, bottom: 40, left: 40 };
 var width = 1000 - margin.left - margin.right;
 var height = 500 - margin.top - margin.bottom;
 
-var xScale = d3.scaleBand().range([0, width]).padding(0.1);
-var yScale = d3.scaleLinear().range([height, 0]);
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", function () {
+        return "translate(" + margin.left + "," + margin.top + ")";
+    });
+
+// Defining some scales, without the domain to remain independent from the data
+var xScale = d3.scaleBand().range([0, width]).padding(0.2);
+var pScaleLegs = d3.scaleLinear().range([50, height * 0.3]);
+var pScaleArms = d3.scaleLinear().range([50, height * 0.3]);
+var pScaleTorso = d3.scaleLinear().range([50, height * 0.3]);
+var pScaleHead = d3.scaleLinear().range([50, height * 0.3]);
 
 function setXScaleDomain(data) {
-  xScale.domain(data.map(function(d) { return d.id; }));
+    xScale.domain(data.map(function (d) { return d.id; }));
 }
 
-function setYScaleDomain(data) {
-    yScale.domain([0, 100]);
+function setPScaleLegs(data) {
+    pScaleLegs.domain([d3.min(data, function (d) { return d.legs; }), d3.max(data, function (d) { return d.legs; })]);
 }
 
-
-function drawBars(data) {
-    var bars = svg.selectAll(".bar").data(data, function(d) { return d.id; });
-    bars.exit().remove();
-
-    bars.enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return xScale(d.id); })
-        .attr("width", xScale.bandwidth())
-        .attr("y", function(d) { return yScale(d.legs); })
-        .attr("height", function(d) { return height - yScale(d.legs); });
+function setPScaleArms(data) {
+    pScaleArms.domain([d3.min(data, function (d) { return d.arms; }), d3.max(data, function (d) { return d.arms; })]);
 }
-// Reading the dataset from the JSON file
+
+function setPScaleTorso(data) {
+    pScaleTorso.domain([d3.min(data, function (d) { return d.torso; }), d3.max(data, function (d) { return d.torso; })]);
+}
+function setPScaleHead(data) {
+    pScaleHead.domain([d3.min(data, function (d) { return d.head; }), d3.max(data, function (d) { return d.head; })]);
+}
+
+function drawPeople(data) {
+
+    setXScaleDomain(data);
+    setPScaleLegs(data);
+    setPScaleArms(data);
+    setPScaleTorso(data);
+    setPScaleHead(data);
+
+    var people = svg.selectAll("g.person")
+        .data(data, function (d) { return d.id; });
+
+    var person = people.enter().append("g")
+        .attr("class", "person")
+        .attr("transform", function (d) {
+            return "translate(" + xScale(d.id) + ",0)";
+        });
+
+    person.each(function (d) {
+        var p = d3.select(this);
+        var person_space = xScale.bandwidth();
+        var centerX = person_space / 2;
+
+        // Legs
+        p.append("rect")
+            .attr("class", "legs")
+            .attr("x", centerX - (person_space * 0.8) / 2)
+            .attr("y", height - pScaleLegs(d.legs))
+            .attr("width", person_space * 0.8)
+            .attr("height", pScaleLegs(d.legs))
+            .style("fill", "blue")
+            .style("stroke", "red")
+            .style("stroke-width", 2);
+
+        p.append("line")
+            .attr("class", "legs-divider")
+            .attr("x1", centerX)                                     // center of the rect
+            .attr("y1", height - pScaleLegs(d.legs))                 // top of the legs
+            .attr("x2", centerX)                                     // same x
+            .attr("y2", height)                                      // bottom of the SVG (“ground”)
+            .attr("stroke", "red")
+            .attr("stroke-width", 2);
+
+        // Arms
+    });
+
+
+};
+
 var data = d3.json("data_cases.json");
 
 // Calling some functions on the dataset to create the visualization
-data.then(function(data) {
-    setXScaleDomain(data);
-    setYScaleDomain(data);
-    drawBars(data);
-    setTimeout(function() {
-        data.sort(function(a, b) {
-            return d3.ascending(a.legs, b.legs);
-        });
-        setXScaleDomain(data);
-        d3.select("body").on("click", function(d) {
-            svg.selectAll(".bar")
-                .transition()
-                .duration(3000)
-                .attr("x", function(d) { return xScale(d.id); });
-        });
-    }, 100);
-}).catch(function(error) {
+data.then(function (data) {
+    drawPeople(data);
+}).catch(function (error) {
     console.error('Error loading the data: ', error);
 });
+
+
